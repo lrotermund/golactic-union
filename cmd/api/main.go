e34c264b3b110aff5f74944c1aeac578fd15d222
Lukas Rotermund <lukas.rotermund@live.de>
2024-12-30 10:32:27 +0100

    fixed main.go license

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,7 +10,7 @@ package main
 // This file is part of golactic-union.
 //
 // golactic-union is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
+// it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
@@ -19,5 +19,5 @@ package main
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
+// You should have received a copy of the GNU Affero General Public License
 // along with golactic-union. If not, see <https://www.gnu.org/licenses/>.
